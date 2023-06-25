Changelog: - Added income_business_amt to the income columns and total income calculation - Fixed isssue where reassignment dict didn't have the right target for Ammavadi - Fix issue where the logic for reassignment was only trying ot move when it was a null - Added moving log - Fixed issue where wrong columns names were being used for assigning the amount values - Added a drop list to the final h1 cleanup where reassignment columns are invalid (unsure if this how we are supposed to do this. - Removed Extraneous print from work hour move - Added method to purge any income > 200000 (I think this should be removed)

pipeline/cleanup_h1.py
@@ -43,6 +43,7 @@ def calculate_h1_income_total(df):
             "h1_income_govt_amt",
             "h1_income_remittances_amt",
             "h1_income_other_amt",
+            "h1_income_business_amt",
         ]
     ].sum(axis=1, skipna=True)
     return df
@@ -131,7 +132,7 @@ def add_exp_other_specify_to_h1_exp_substance_amt(df):
 
 
 INCOME_REASSIGN_DICT = {
-    "Ammavadi": "h1_income_govt_amt",
+    "Ammavadi": "h1_income_govt",
     "Auto": "h1_income_business",
     "Bullero (auto)": "h1_income_business",
     "Chakali ( battalu wash)": "h1_income_business",
@@ -156,29 +157,53 @@ INCOME_REASSIGN_DICT = {
 
 
 def reassign_h1_income_other_specify(df):
-    def fix_row(row, lookup_table):
-        # Look at the content and figure out where the stuff has to go
-        specified_other_category = row["h1_income_other_specify"]
-        # Check if this is somthing we need to do
-        if specified_other_category in lookup_table.key() and pd.isna(
-            specified_other_category is False
-        ):
-            value = row["h1_income_other_amt"]
-            reassignment_column_name = lookup_table[specified_other_category]
-            if row[reassignment_column_name].isna():
-                row[reassignment_column_name] = value
+    def fix_row(row, lookup_table, drop_list):
+        if pd.isna(row["h1_income_other_specify"]) is False:
+            # Look at the content and figure out where the stuff has to go
+            specified_other_category = row["h1_income_other_specify"]
+            # Check if this is somthing we need to do
+            value = float(row["h1_income_other_amt"])
+            if specified_other_category in lookup_table:
+                reassignment_category_column_name = lookup_table[
+                    specified_other_category
+                ]
             else:
-                row[reassignment_column_name] += value
+                # Add HHID to droplist
+                drop_list.append(row["hhid"])
+                return row
+            reassignment_amount_column_name = f"{reassignment_category_column_name}_amt"
+            # Set category to yes
+            row[reassignment_category_column_name] = "Yes"
+            if reassignment_amount_column_name not in row.index:
+                row[reassignment_amount_column_name] = value
+            elif pd.isna(row[reassignment_amount_column_name]):
+                row[reassignment_amount_column_name] = value
+            else:
+                row[reassignment_amount_column_name] += value
 
             # Blank out the old columns
-            row["h1_income_substance_amt"] == row["h1_income_other_amt"]
             row["h1_income_other_amt"] == np.nan
             row["h1_income_other_specify"] = np.nan
             row["h1_income_other"] = "No"
+
+            print(
+                f"Moved income_other_amt to {reassignment_amount_column_name} for HHID: {row['hhid']}"
+            )
 
         return row
 
     # Generic whitespace cleanup
     df["h1_income_other_specify"] = df["h1_income_other_specify"].str.strip()
+    drop_list = []
+    df = df.apply(
+        fix_row, lookup_table=INCOME_REASSIGN_DICT, drop_list=drop_list, axis=1
+    )
 
-    df = df.apply(fix_row, lookup_table=INCOME_REASSIGN_DICT)
+    # Drop all rows with the following HHIDs
+    print(
+        "Dropping the following rows becuase income_other_specify is not found in reassignment lookup table:",
+        drop_list,
+    )
+    df = df[~df["hhid"].isin(drop_list)]
+
+    return df

pipeline/cleanup_h3.py
@@ -1,6 +1,17 @@
 import numpy as np
 import pandas as pd
 
+from pipeline.utils import purge_outliers
+
+
+def purge_h3_outliers(df):
+    mask = df["h3_pesticide"] > 200000
+    filtered_df = df[~mask]
+    dropped_ids = df[mask]["hhid"].unique()
+    for d_id in dropped_ids:
+        print(f"Dropped HHID : {d_id}")
+    return filtered_df
+
 
 def calculate_h3_ownland(df):
     # First cleanup the individual land data
@@ -33,7 +44,7 @@ def calculate_h3_cultivateland(df):
     raise NotImplementedError()
 
 
-def calculate_h3_fullyorganic(df):
+def classify_h3_fullyorganic(df):
     # Note that these can't be created until you've calculated organic status for each plot individually!
     # Generate binary variable: fully organic if all plots of land reported by farmer are organic, not organic otherwise
 
@@ -42,8 +53,9 @@ def calculate_h3_fullyorganic(df):
     raise NotImplementedError()
 
 
-def calculate_h3_fullyorganic2(df):
+def classify_h3_fullyorganic2(df):
     # Generate categorical variable: fully organic if all plots of land reported by farmer are organic, partially organic if at least one plot is organic, not organic otherwise
+    # Do all the other things first
     raise NotImplementedError()
 
 
@@ -97,14 +109,89 @@ def calculate_h3_plot3organic_calc(df):
 
 def calcualte_h3_costofcult(df):
     # sum of all cost variables below, including h3_otherorganic and h3_madecost, and seed cost for all five crops
-    raise NotImplementedError()
+    """
+    h3_soil
+    h3_fertilizer
+    h3_manure
+    h3_pesticide
+    h3_jeevamrutam
+    h3_biocides
+    h3_mechanicalpest
+    h3_diesel
+    h3_electricity
+    h3_mulchmaterial
+    h3_hlabour_mulch
+    h3_hlabour_plough
+    h3_hlabour_sow
+    h3_hlabour_weed
+    h3_hlabour_prune
+    h3_hlabour_spray
+    h3_hlabour_harvest
+    h3_labor
+    h3_irrigation
+    h3_maintenance
+    h3_machinery
+    h3_lease
+    """
+    # Calculate total seed and seed treat costs first
+    df = calculate_h3_seedcosttotal(df)
+    df = calculate_h3_seedtreattotal(df)
+
+    df["h3_costofcult"] = df[
+        [
+            "h3_soil",
+            "h3_fertilizer",
+            "h3_manure",
+            "h3_pesticide",
+            "h3_jeevamrutam",
+            "h3_biocides",
+            "h3_mechanicalpest",
+            "h3_diesel",
+            "h3_electricity",
+            "h3_mulchmaterial",
+            "h3_hlabour_mulch",
+            "h3_hlabour_plough",
+            "h3_hlabour_sow",
+            "h3_hlabour_weed",
+            "h3_hlabour_prune",
+            "h3_hlabour_spray",
+            "h3_hlabour_harvest",
+            # "h3_labor",
+            "h3_irrigation",
+            "h3_maintenance",
+            "h3_machinery",
+            "h3_lease",
+            "h3_otherorganic",
+            "h3_madecost",
+            "h3_seedcosttotal",
+            "h3_seedtreattotalcost",
+        ]
+    ].sum(axis=1, skipna=True)
+    return df
 
 
 def calculate_h3_seedcosttotal(df):
     # sum of all seed costs (five columns, one for each crop): h3_cropxseedcost
-    raise NotImplementedError()
+    # TODO: Replace with regex later
+    df["h3_seedcosttotal"] = (
+        df["h3_crop1seedcost"].fillna(0)
+        + df["h3_crop2seedcost"].fillna(0)
+        + df["h3_crop3seedcost"].fillna(0)
+        + df["h3_crop4seedcost"].fillna(0)
+        + df["h3_crop5seedcost"].fillna(0)
+    )
+    return df
 
 
 def calculate_h3_seedtreattotal(df):
     # sum of all seed treatment costs (five columns, one for each crop): h3_cropxseedtreatcost
-    raise NotImplementedError()
+    # TODO: Replace with regex later
+    df["h3_seedtreattotalcost"] = (
+        df["h3_crop1seedtreatcost"].fillna(0)
+        + df["h3_crop2seedtreatcost"].fillna(0)
+        + df["h3_crop3seedtreatcost"].fillna(0)
+        + df["h3_crop4seedtreatcost"].fillna(0)
+        + df["h3_crop5seedtreatcost"].fillna(0)
+    )
+
+    return df