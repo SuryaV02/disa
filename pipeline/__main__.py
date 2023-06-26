82139440024bb70924253dbcc45913ca0d423447
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2023-06-26 05:32:42 +0000

    Added the new cleanup items to the list

--- pipeline/__main__.py
+++ pipeline/__main__.py
@@ -10,7 +10,7 @@ from pipeline.cleanup_h1 import (
     update_h1_exp_rituals_amt,
     update_h1_exp_water_amt,
 )
-from pipeline.cleanup_h3 import calculate_h3_farmsize
+from pipeline.cleanup_h3 import calcualte_h3_costofcult, calculate_h3_farmsize, purge_h3_outliers
 from pipeline.cleanup_s4 import (
     merge_s4_cluster,
     cleanup_s4_hhcategory,
@@ -24,7 +24,9 @@ import pandas as pd
 def main():
     # merged_df = merge_redcap_event_rows('/root/sandbox/disa/data_v0004.tsv')
     merged_df = pd.read_csv(
-        "/root/sandbox/disa/data_merged_21-06-2023-10:36:19.tsv", sep="\t", header=0
+        "/root/sandbox/disa/data_merged_23-06-2023-22:32:11.tsv",
+        sep="\t",
+        header=0,
     )
 
     # s4 Study
@@ -51,12 +53,14 @@ def main():
     cleaned_df = calculate_work_totals(cleaned_df)
 
     # h3 Study
+    cleaned_df = purge_h3_outliers(cleaned_df)
     # This one computes all the dependencies automatically
     # 1. calculate_h3_ownland()
     # 2. calculate_h3_plot1land()
     # 3. calculate_h3_plot2land()
     # 4. calculate_h3_plot3land()
     cleaned_df = calculate_h3_farmsize(cleaned_df)
+    cleaned_df = calcualte_h3_costofcult(cleaned_df)
 
     cleaned_df.to_csv("/root/sandbox/disa/data_cleaned.tsv", sep="\t", index=False)
 
