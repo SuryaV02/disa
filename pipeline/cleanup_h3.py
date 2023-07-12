560e4dc30c5b797dc0e4f9bd5d314c964f2cd4b8
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2023-07-12 09:24:38 +0000

    moved the json fields file to a dedicated directory

--- pipeline/cleanup_h3.py
+++ pipeline/cleanup_h3.py
@@ -331,6 +331,7 @@ def calculate_h3_cropNland(df):
 
 def calculate_h3_cropN_yield_normalized(df):
     ## TODO: Wrap up the rield calculations
+    # Unit = kg/acre 
     def calcualte_yield_in_kg(df, N: int):
         df[f"h3_crop{N}_yield_in_kg"] = df[f"h3_crop{N}yield"]
         return df
